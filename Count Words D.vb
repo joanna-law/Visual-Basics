960f64b02522f7b603ce8397d4c113b366d6b3c8
Joanna Lau Ah Wing <j_lauahw@encs.concordia.ca>
2019-01-12 23:29:27 -0500

    String manipulation

---
+++ Count Words D.vb
@@ -0,0 +1,24 @@
+﻿'Program that counts the number of words in a sentence that start with the letter d
+Module Module1
+
+    Sub Main()
+        Dim count As Integer
+        Dim sentence As String
+        count = 0
+        Console.WriteLine("Input your sentence.")
+        sentence = Console.ReadLine
+        If Mid(sentence, 1) = "D" Then
+            count = count + 1
+        End If
+        For i = 1 To Len(sentence)
+            If Mid(sentence, i) = "D" Or Mid(sentence, i) = "d" Then
+                If Mid(sentence, i - 1) = " " Then
+                    count = count + 1
+                End If
+            End If
+        Next
+        Console.WriteLine("There are " & count & " words starting with d.")
+        Console.ReadLine()
+    End Sub
+
+End Module
